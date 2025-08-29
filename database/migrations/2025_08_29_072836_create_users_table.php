a553a06287c0b66896cf189b42b255d8de2e39e2
huykunptit <huykunptit0710@gmail.com>
2025-08-29 15:29:23 +0700

    Add migrate User,Category, Product, Order, Payment, Review, Cart

--- database/migrations/2025_08_29_072836_create_users_table.php
+++ database/migrations/2025_08_29_072836_create_users_table.php
@@ -9,24 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number', 15)->nullable();
+            $table->enum('role', ['buyer', 'seller'])->default('buyer');
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
+
 };
